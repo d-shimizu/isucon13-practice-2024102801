0b782bd20ecd5598f850eef256f9ccf855ee6cf5
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2025-01-06 14:06:53 +0000

    icons テーブルの user_id にIndexを追加した

--- webapp/sql/initdb.d/10_schema.sql
+++ webapp/sql/initdb.d/10_schema.sql
@@ -15,7 +15,8 @@ CREATE TABLE `icons` (
   `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `user_id` BIGINT NOT NULL,
   `image` LONGBLOB NOT NULL,
-  `hash` TEXT NOT NULL
+  `hash` TEXT NOT NULL,
+  INDEX `idx_icons_user_id` (`user_id`)
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 -- ユーザごとのカスタムテーマ
